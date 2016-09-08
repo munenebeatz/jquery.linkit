72b12c94db665da75638cce90c12cc91d90f97ac
Munenebeatz <md@cordelia.co.ke>
2016-09-08 16:00:33 +0300

    plugIn init

---
+++ jquery.linkIt.js
@@ -0,0 +1,11 @@
+/*
+* name: LinkedIt
+* author: Brian Munene
+* version: 0.1.0
+* licence: MIT
+* */
+
+
+(function ($) {
+
+})(jQuery);
--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 /**
  * Created by CORDELIA on 07-Sep-16.
  */
+
+(function ($) {
+    $('*').on('click', function () {
+        var typ = $(this).prop('tagName');
+        console.log(typ);
+    });
+})(jQuery);
